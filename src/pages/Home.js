99c57656ad516b5151d39fbe210741709b035ba3
Gabe Vee <0gabevee0@gmail.com>
2023-01-18 19:11:12 -0700

    added initial code for pagination

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,47 @@
 import { useState, useEffect } from "react";
-import { getAllPlayers } from "../api/index";
-import PlayerList from "../components/PlayerList";
+import axios from "axios";
+// import { getAllPlayers } from "../api/index";
+// import PlayerList from "../components/PlayerList";
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "86b4788c35mshbfeccd3463252bbp1ce753jsn40224f9087e4",
+    "X-RapidAPI-Host": "free-nba.p.rapidapi.com",
+  },
+};
 
 const Home = () => {
-  const [results, setResults] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+  // const [results, setResults] = useState([]);
 
-  const search = async (query) => {
-    const response = await getAllPlayers(query);
-    setResults(response);
-  };
+  // const search = async (query) => {
+  //   const response = await getAllPlayers(query);
+  //   setResults(response);
+  // };
 
   useEffect(() => {
-    search("players");
-  }, []);
+    // search("players");
+    const fetchPlayers = async () => {
+      setLoading(true);
+      const res = await axios.get(
+        "https://free-nba.p.rapidapi.com/players",
+        options
+      );
+      setPosts(res.data);
+      setLoading(false);
+    };
 
+    fetchPlayers();
+  }, []);
+  console.log(posts);
   return (
     <main className="card-container">
       <div className="col-12 col-md-10 my-3">
-        <PlayerList results={results.data} />
+        {/* <PlayerList results={results.data} /> */}
       </div>
     </main>
   );
