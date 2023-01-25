17b40aa3ce44a7dee31944a579d2542d509a733f
Gabe Vee <0gabevee0@gmail.com>
2023-01-24 21:22:14 -0700

    Added stats page and list components

---
+++ src/components/StatsList/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
+// In our return method, we use the map method to return a new array of `li` and `img` elements that are specific to each search result
+function StatsList({ posts, loading }) {
+  if (loading) {
+    return <h3>...Loading</h3>;
+  }
+  return (
+    <Row xs={1} md={3} className="g-4">
+      {posts.map((stats) => (
+        <Col key={stats.id}>
+          <Card className="bg-main">
+            <Card.Body>
+              <Card.Title className="text-light">stats</Card.Title>
+              <div></div>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
+}
+
+export default StatsList;
---
+++ src/pages/Stats.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import GameList from "../components/StatsList/index";
+import { Pagination } from "../components/Pagination";
+
+const Stats = () => {
+  const [posts, setPosts] = useState([]);
+  const [meta, setMeta] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(30);
+
+  const options = {
+    method: "GET",
+    url: "https://free-nba.p.rapidapi.com/stats",
+    params: { page: currentPage.toString(), per_page: postsPerPage.toString() },
+    headers: {
+      "X-RapidAPI-Key": "86b4788c35mshbfeccd3463252bbp1ce753jsn40224f9087e4",
+      "X-RapidAPI-Host": "free-nba.p.rapidapi.com",
+    },
+  };
+
+  useEffect(() => {
+    const fetchPlayers = async () => {
+      setLoading(true);
+      const res = await axios.request(options);
+      setPosts(res.data.data);
+      setMeta(res.data.meta);
+      setLoading(false);
+    };
+
+    fetchPlayers();
+  }, []);
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  //TODO: add pagination to game page
+
+  return (
+    <main className="card-container">
+      <div className="col-12 col-md-10 my-3">
+        <StatsList posts={currentPosts} loading={loading} />
+      </div>
+    </main>
+  );
+};
+
+export default Stats;
