09937e47a5f9db0cbd695a0130fe719b1cc19065
Gabe Vee <0gabevee0@gmail.com>
2023-02-01 23:03:25 -0700

    Pagination v2 work in progress

--- src/pages/Players.js
+++ src/pages/Players.js
@@ -8,20 +8,17 @@ const Players = () => {
   const [meta, setMeta] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(27);
+  // const [postsPerPage] = useState();
 
-  meta.current_page = currentPage;
-  meta.next_page = currentPage + 1;
-
-  const options = {
-    method: "GET",
-    url: "https://free-nba.p.rapidapi.com/players",
-    params: { page: currentPage, per_page: 27 },
-    headers: {
-      "X-RapidAPI-Key": "86b4788c35mshbfeccd3463252bbp1ce753jsn40224f9087e4",
-      "X-RapidAPI-Host": "free-nba.p.rapidapi.com",
-    },
-  };
+  // const options = {
+  //   method: "GET",
+  //   url: "https://free-nba.p.rapidapi.com/players",
+  //   params: { page: currentPage, per_page: 27 },
+  //   headers: {
+  //     "X-RapidAPI-Key": "86b4788c35mshbfeccd3463252bbp1ce753jsn40224f9087e4",
+  //     "X-RapidAPI-Host": "free-nba.p.rapidapi.com",
+  //   },
+  // };
 
   useEffect(() => {
     const fetchPlayers = async () => {
@@ -31,30 +28,29 @@ const Players = () => {
       setMeta(res.data.meta);
       setLoading(false);
     };
-
     fetchPlayers();
   }, []);
 
   console.log(meta);
-  // console.log(posts);
+  console.log(posts);
 
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+  // const indexOfLastPost = currentPage * postsPerPage;
+  // const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  // const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
 
-  console.log(currentPosts);
+  meta.current_page = currentPage;
+  meta.next_page = currentPage + 1;
   return (
     <main className="card-container">
       <div className="col-12 col-md-10 my-3">
-        <PlayerList posts={currentPosts} loading={loading} />
+        <PlayerList posts={posts} loading={loading} />
         <PaginationControl
-          page={currentPage}
+          page={meta.current_page}
           between={4}
           total={meta.total_count}
           limit={meta.per_page}
           changePage={(page) => {
             setCurrentPage(page);
-            console.log(page);
           }}
           ellipsis={1}
         />
